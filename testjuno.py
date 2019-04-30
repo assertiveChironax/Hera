f94f2c6743526b350e78caa90ff97f77480d9b5e
assertiveChironax <assertiveChironax@gmail.com>
2019-04-30 02:12:02 -0700

    Update testjuno.py

--- testjuno.py
+++ testjuno.py
@@ -9,6 +9,10 @@ luck = luck
 lucky = luck
 bangy = bang
 
+class Help:
+    """This is the list of commands currently available."""
+bot.add_cog(Help())
+
 BOT_PREFIX = ("!")
 
 client = Bot(command_prefix=BOT_PREFIX)
@@ -16,7 +20,7 @@ client = Bot(command_prefix=BOT_PREFIX)
 #Yes/No
 @client.command(name="mom",
                 description="Determine the answer to yes-or-no style questions.",
-                brief="Why don't you ask your Mother?",
+                brief="Why do you not you ask your Mother?",
                 aliases=[ 'mother', 'mommy', 'mama', 'ma' ],
                 pass_context=True)
 async def mom(context):
@@ -28,17 +32,19 @@ async def mom(context):
     ]
     #await client.say(random.choice(possible_responses)) for no @
     await client.say(context.message.author.mention + "," + random.choice(possible_responses))
+  bot.add_cog(Help())
 
 #Dice Roller v1
 @client.command(name="roll",
-                description="Rolls a single d20 die; sorry Vee isn't smart enough to let you roll multiple dice",
+                description="Rolls a single d20 die. Apologies, Vee is not smart enough to let you roll multiple dice",
                 brief="Does this need explaining?",
                 pass_context=True)
 async def roll(context):
     number = random.randint(1, 21)
     await client.say("You have rolled a " + str(number) + ", " + context.message.author.mention + ".")
+  bot.add_cog(Help())
 
- #Russian Roulette
+#Russian Roulette
 @client.command(name="roulette",
                 description="For each use, the trigger is pulled until the gun goes off. Use the reload command after it does do or at your own leisure.",
                 brief="Are you currently feeling lucky, punk?",
@@ -55,6 +61,37 @@ async def roulette(context):
     await client.say(context.message.author.mention + " Click.")
     luck = luck + 1
     return (luck)
+  bot.add_cog(Help())
+  
+#Reload
+@client.command(name="reload",
+                description="For each use after the gun goes off in the roulette game. Or if you would like some fresh luck.",
+                brief="Reloads the gun.",
+                aliases=[ 'r' ],
+                pass_context=True)
+async def reload(context):
+  global lucky
+  luck = random.randint(1,6)
+  await client.say(context.message.author.mention + " I  have emptied the firearm and inserted one bullet into a random chamber.")
+  return (luck)
+  bot.add_cog(Help())
+
+#Peek
+@client.command(name="peek",
+                description="If you are ever too nervous about pulling the trigger, you can always look through the chambers.",
+                brief="Naughty, naughty.",
+                pass_context=True)
+async def peek(context):
+  global lucky
+  global bangy
+  lucky = luck
+  bangy = bang
+  peek = (bang - luck) + 1
+  if peek == 1:
+    await client.say(context.message.author.mention + " Oh dear.") 
+  if lucky != bangy:
+    await client.say(context.message.author.mention + " how cowardly. The bullet is " + str(peek) + " chamber(s) away.")
+  bot.add_cog(Help())
                 
 client.run(os.getenv("TOKEN"))
 
