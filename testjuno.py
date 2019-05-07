51de604d36e080fbf02581cbd78a20e84e3c7493
assertiveChironax <assertiveChironax@gmail.com>
2019-05-07 13:20:02 -0700

    Update testjuno.py

--- testjuno.py
+++ testjuno.py
@@ -88,7 +88,7 @@ async def peek(context):
                 brief="Would you like Mother to choose for you?",
                 aliases=[ 'pick', 'select' ],
                 pass_context=True)
-async def peek(context, *choices):
+async def choose(context, *choices):
   if len(choices) < 2:
     await client.say(context.message.author.mention + " There are not enough choices.) 
   else:
